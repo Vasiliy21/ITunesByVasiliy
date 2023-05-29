b6a081de4c32509766cc5b9ed53822db1f54fea7
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2023-05-29 21:24:36 +0300

    Initial Commit

---
+++ ITunesByVasiliy/ITunesByVasiliyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesByVasiliyApp.swift
+//  ITunesByVasiliy
+//
+//  Created by Vasiliy on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ITunesByVasiliyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
